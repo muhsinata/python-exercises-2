d87a565f925e03306af8d5273eb13027433c7320
muhsinata <muhsinatadurak@gmail.com>
2022-08-02 14:01:46 +0300

    add new function to class

--- first-class-ex/main.py
+++ first-class-ex/main.py
@@ -3,8 +3,18 @@ class User:
     def __init__(self, username, id):
         self.username = username
         self.id = id
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
-user = User("angela", "001")
-print(user.username)
-print(user.id)
+user1 = User("Mike", "001")
+user2 = User("Steve", "002")
+
+user1.follow(user2)
+
+print(f"user1 is following {user1.following} people.")
+print(f"user2 is being followed by {user2.followers} people.")
