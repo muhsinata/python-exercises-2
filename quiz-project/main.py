0a8dd84934167d5c7e38ff9b87d26781050afea7
muhsinata <muhsinatadurak@gmail.com>
2022-08-04 12:04:44 +0300

    fix error of code

--- quiz-project/main.py
+++ quiz-project/main.py
@@ -2,13 +2,13 @@ from data import question_data
 from question_model import Question
 
 score = 0
-question = 0
+number_of_question = 0
 for question in question_data:
-    question = Question(question["text"], question["answer"])
+    question = Question(question["text"], question["answer"].lower())
     print(question.text)
-    answer = input("True or False: ")
-    question += 1
+    answer = input("True or False: ").lower()
+    number_of_question += 1
     if answer == question.answer:
         score += 1
-        print(f"Success: {score}/{question}")
+    print(f"Success: {score}/{number_of_question}")
 
