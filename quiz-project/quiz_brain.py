d7c55475bb361a6c06a0147078e492f4cb5d7164
muhsinata <muhsinatadurak@gmail.com>
2022-08-05 10:42:19 +0300

    finish project

--- quiz-project/quiz_brain.py
+++ quiz-project/quiz_brain.py
@@ -3,20 +3,21 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
-    def check_answer(self, user_answer, correct_answer):
-        if 
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
 
     def current_question(self):
         chosen_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {chosen_question.text} (True/False)?: ")
+        self.check_answer(user_answer, chosen_question.answer)
 
-    def still_has_questions(self):
-        return self.question_number < len(self.question_list)
-
-
-
-
-
-
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print(f"You're right! {self.score}/{self.question_number}")
+        else:
+            print(f"You're wrong! {self.score}/{self.question_number}")
+        print("\n")
