ab31050dae9cdd243734090f20f644a54d75b5bc
muhsinata <muhsinatadurak@gmail.com>
2022-08-06 12:15:38 +0300

    learn how to make random walk

--- timmy-the-turtle/main.py
+++ timmy-the-turtle/main.py
@@ -1,18 +1,34 @@
 import turtle as t
+import random
 
 tim = t.Turtle()
-tim.shape("turtle")
-
-for sides in range(3, 10):
-    for times in range(sides):
-        tim.forward(100)
-        tim.right(360/sides)
-
-
-
-
-
-
-
 screen = t.Screen()
+
+screen.colormode(255)
+tim.shape("turtle")
+tim.pensize(10)
+tim.speed(10)
+
+colors = [1, 2, 3]
+turn = [90, -90, 180, 0]
+
+for _ in range(150):
+
+    for color in range(3):
+        random_rgb_value = random.randint(1, 255)
+        colors[color] = random_rgb_value
+
+    tim.pencolor(colors[0], colors[1], colors[2])
+    tim.right(random.choice(turn))
+    tim.forward(22)
+
 screen.exitonclick()
+
+
+
+
+
+
+
+
+
