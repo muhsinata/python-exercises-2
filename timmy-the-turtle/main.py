8aaf79ede8469fe7021c768225f3de286b3943d2
muhsinata <muhsinatadurak@gmail.com>
2022-08-07 12:38:56 +0300

    finish dot-painting project

--- timmy-the-turtle/main.py
+++ timmy-the-turtle/main.py
@@ -6,33 +6,34 @@ tim = t.Turtle()
 screen = t.Screen()
 tim.speed("fastest")
 
-colors = cg.extract("hirst.jpg", 2**32)
-color_list = []
+colors = cg.extract("hirst.jpg", 30)
+color_list = [(224, 150, 101), (176, 156, 101), (71, 92, 108), (138, 133, 131), (238, 215, 161),
+              (247, 239, 210), (205, 139, 98),  (239, 134, 119), (160, 231, 125), (130, 182, 217),
+              (240, 130, 172), (188, 171, 252), (85, 123, 63), (255, 217, 100), (247, 231, 195)]
+
 screen.colormode(255)
 
+# def extract_colors():
+#
+#     for _ in range(6):
+#         color_list.append(tuple(colors[_].rgb))
+#
+#
+# extract_colors()
 
-def extract_colors():
-
-    for _ in range(6):
-        color_list.append(tuple(colors[_].rgb))
-
-
-extract_colors()
-
+tim.hideturtle()
 tim.penup()
 tim.right(180)
-tim.forward(200)
+tim.forward(250)
 tim.left(90)
-tim.forward(200)
+tim.forward(250)
 tim.left(90)
 
 for _ in range(10):
     for line in range(10):
-        tim.fillcolor(random.choice(color_list))
-        tim.begin_fill()
-        tim.color(random.choice(color_list))
-        tim.circle(10)
-        tim.end_fill()
+        dot_color = random.choice(color_list)
+        tim.dot(20, dot_color)
+        tim.color(dot_color)
         tim.penup()
         tim.forward(50)
     tim.right(180)
@@ -42,4 +43,3 @@ for _ in range(10):
     tim.right(90)
 
 screen.exitonclick()
-
