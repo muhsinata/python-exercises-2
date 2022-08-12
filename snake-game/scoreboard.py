65408b8c4a86d2266c6be44201b476de7aaee228
muhsinata <muhsinatadurak@gmail.com>
2022-08-12 13:04:50 +0300

    fix: add scoreboard file

---
+++ snake-game/scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Arial", 10, "bold")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.coordinate_x = 0
+        self.coordinate_y = 270
+        self.score = 0
+        self.goto(self.coordinate_x, self.coordinate_y)
+        self.score_screen()
+
+    def score_screen(self):
+        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
