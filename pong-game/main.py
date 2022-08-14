2c315ebfbcb3afe5cd3b85a308d30e57740b4eaf
muhsinata <muhsinatadurak@gmail.com>
2022-08-14 13:55:38 +0300

    feat: add Scoreboard class

--- pong-game/main.py
+++ pong-game/main.py
@@ -1,3 +1,5 @@
 import game_screen
+import scoreboard
 
+scoreboard = scoreboard.Scoreboard()
 screen = game_screen.GameScreen()
---
+++ pong-game/scoreboard.py
@@ -0,0 +1,35 @@
+import turtle
+
+SCORE_COLOR = "black"
+FONT = ("Arial", 60, "normal")
+
+
+class Scoreboard:
+
+    def __init__(self):
+        self.user_x_coordinate = -100
+        self.user_y_coordinate = 150
+        self.computer_x_coordinate = 100
+        self.computer_y_coordinate = 150
+        self.user_score = 0
+        self.computer_score = 0
+        self.user_score_turtle = turtle.Turtle()
+        self.user_score_turtle.color("white")
+        self.user_score_turtle.speed("fastest")
+        self.user_score_turtle.penup()
+        self.user_score_turtle.hideturtle()
+        self.computer_score_turtle = turtle.Turtle()
+        self.computer_score_turtle.color("white")
+        self.computer_score_turtle.speed("fastest")
+        self.computer_score_turtle.penup()
+        self.computer_score_turtle.hideturtle()
+        self.score_places()
+        self.scores()
+
+    def score_places(self):
+        self.user_score_turtle.goto(self.user_x_coordinate, self.user_y_coordinate)
+        self.computer_score_turtle.goto(self.computer_x_coordinate, self.computer_y_coordinate)
+
+    def scores(self):
+        self.user_score_turtle.write(arg=self.user_score, align="center", font=FONT)
+        self.computer_score_turtle.write(arg=self.computer_score, align="center", font=FONT)
