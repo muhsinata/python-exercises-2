61d49afedf65d6908842aa3f77b35477252c9882
muhsinata <muhsinatadurak@gmail.com>
2022-08-16 13:52:22 +0300

    feat: add game's screen settings

---
+++ pong-game-v2.0/main.py
@@ -0,0 +1,18 @@
+import turtle
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = turtle.Screen()
+
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.bgcolor("black")
+screen.title("Pong")
+
+
+
+
+
+
+
+screen.exitonclick()
