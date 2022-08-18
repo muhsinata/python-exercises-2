733d1ce195ee2c13ea44bef84d697ae0597e004e
muhsinata <muhsinatadurak@gmail.com>
2022-08-18 20:53:19 +0300

    feat: add turtle shape

--- turtle-crossing/player.py
+++ turtle-crossing/player.py
@@ -6,9 +6,10 @@ FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
-    
+
     def __init__(self):
         super().__init__()
+        self.shape("turtle")
         self.penup()
         self.goto(STARTING_POSITION)
         self.setheading(90)
