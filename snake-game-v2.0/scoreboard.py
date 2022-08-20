31cb9c3d08e781f8e9f147afdc55bac0ca2f1eb0
muhsinata <muhsinatadurak@gmail.com>
2022-08-20 12:58:46 +0300

    chore: delete unnecessary code

--- snake-game-v2.0/scoreboard.py
+++ snake-game-v2.0/scoreboard.py
@@ -29,10 +29,6 @@ class Scoreboard(Turtle):
         self.score = 0
         self.update_scoreboard()
 
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
-
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
