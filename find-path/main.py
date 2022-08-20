5fc6ed8be599ba4cc9d63fa374026e200f90e1e8
muhsinata <muhsinatadurak@gmail.com>
2022-08-20 22:59:29 +0300

    feat: create personalized letters

--- find-path/main.py
+++ find-path/main.py
@@ -1,11 +1,18 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
 
-with open("C:/Users/MSI/PycharmProjects/python-exercises-2/find-path/Input/Names/invited_names.txt", "r") as names:
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("Input/Names/invited_names.txt", "r") as names:
     name_list = names.read().splitlines()
+
+for name in name_list:
+    with open(f"Output/ReadyToSend/{name}.txt", "w") as letter:
+        with open("Output/ReadyToSend/example.txt") as replace_name:
+            content = replace_name.read()
+            content = content.replace("Aang", name)
+            letter.write(content)
