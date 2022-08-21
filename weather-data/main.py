bff2c3a5de354f56fd2652d4b4733d382cbcd094
muhsinata <muhsinatadurak@gmail.com>
2022-08-21 15:18:14 +0300

    feat: extract temperatures with pandas

--- weather-data/main.py
+++ weather-data/main.py
@@ -1,11 +1,15 @@
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+# print(temperatures)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
+import pandas
 
-print(temperatures)
-
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
