f2d8808a0c2995aa3869ff6470abb05ca6db1a15
muhsinata <muhsinatadurak@gmail.com>
2022-08-21 16:58:46 +0300

    feat: create a dataframe from a dict

--- weather-data/main.py
+++ weather-data/main.py
@@ -9,14 +9,23 @@
 #
 # print(temperatures)
 
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
+# temp_data = data["temp"].to_list()
+# average_temp = data["temp"].mean()
+# max_temp = data["temp"].max()
+#
+# max_temp_row = data[data.temp == data.temp.max()]
+#
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-temp_data = data["temp"].to_list()
-average_temp = data["temp"].mean()
-max_temp = data["temp"].max()
+data_dict = {
+    "students": ["Steve", "Bob", "Emma"],
+    "scores": [85, 75, 95],
+}
 
-max_temp_row = data[data.temp == data.temp.max()]
-
-monday = data[data.day == "Monday"]
-monday_temp = int(monday.temp)
+data = pandas.DataFrame(data_dict)
