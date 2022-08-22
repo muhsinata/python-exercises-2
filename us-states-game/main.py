13565e4c5fc16e14dff5f1d3ef217f26d8736698
muhsinata <muhsinatadurak@gmail.com>
2022-08-22 13:43:02 +0300

    feat: add exit command

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -13,11 +13,15 @@ data = pandas.read_csv("50_states.csv")
 
 right_guesses = 0
 keep_guessing = True
+guessed_states = []
 
 while keep_guessing:
 
     answer_state = screen.textinput(title=f"{right_guesses}/50 States Correct", prompt="What's state's name?").title()
     answer_check = data[data.state == answer_state].empty
+
+    if answer_state == "Exit":
+        break
 
     if not answer_check:
 
@@ -25,6 +29,7 @@ while keep_guessing:
         state_x = int(state_row.x)
         state_y = int(state_row.y)
         right_guesses += 1
+        guessed_states.append(answer_state)
 
         write_states.find_state(state_x, state_y)
         write_states.write_state_name(answer_state)
