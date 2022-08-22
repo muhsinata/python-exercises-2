2615af5992c435aeb90e810127d124e85873bd95
muhsinata <muhsinatadurak@gmail.com>
2022-08-22 13:19:24 +0300

    feat: write the names of guessed states on the map

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -1,7 +1,9 @@
 import turtle
 import pandas
+import write_states
 
 screen = turtle.Screen()
+write_states = write_states.WriteState()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
@@ -14,11 +16,19 @@ keep_guessing = True
 
 while keep_guessing:
 
-    answer_state = screen.textinput(title=f"{right_guesses/50} States Correct", prompt="What's state's name?").title()
+    answer_state = screen.textinput(title=f"{right_guesses}/50 States Correct", prompt="What's state's name?").title()
     answer_check = data[data.state == answer_state].empty
 
     if not answer_check:
-        
+
+        state_row = data[data.state == answer_state]
+        state_x = int(state_row.x)
+        state_y = int(state_row.y)
+        right_guesses += 1
+
+        write_states.find_state(state_x, state_y)
+        write_states.write_state_name(answer_state)
+
 
 
 
---
+++ us-states-game/write_states.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+FONT = ("Courier", 8, "normal")
+
+
+class WriteState(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+
+    def find_state(self, x, y):
+        self.goto(x, y)
+
+    def write_state_name(self, state_name):
+        self.write(state_name, align="center", font=FONT)
+
