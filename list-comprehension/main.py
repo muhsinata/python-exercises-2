e3ce685223bf1b2a126b351fc3aa8afa99ad2663
muhsinata <muhsinatadurak@gmail.com>
2022-08-23 20:58:19 +0300

    feat: find common numbers in two data files

--- list-comprehension/main.py
+++ list-comprehension/main.py
@@ -1,6 +1,18 @@
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-squared_numbers = [number ** 2 for number in numbers]
-even_numbers = [number for number in numbers if number % 2 == 0]
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# squared_numbers = [number ** 2 for number in numbers]
+# even_numbers = [number for number in numbers if number % 2 == 0]
+#
+# print(squared_numbers)
+# print(even_numbers)
 
-print(squared_numbers)
-print(even_numbers)
+######################################################################
+
+with open("file1.txt") as file_1:
+    file_1_numbers = file_1.read().splitlines()
+
+with open("file2.txt") as file_2:
+    file_2_numbers = file_2.read().splitlines()
+
+common_numbers = [int(number) for number in file_1_numbers if number in file_2_numbers]
+
+print(common_numbers)
