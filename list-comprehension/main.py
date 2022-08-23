9269422d7d096d75328111ed83155734a756da95
muhsinata <muhsinatadurak@gmail.com>
2022-08-23 20:38:50 +0300

    feat: get even numbers w/ list comprehension

--- list-comprehension/main.py
+++ list-comprehension/main.py
@@ -1,5 +1,6 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 squared_numbers = [number ** 2 for number in numbers]
+even_numbers = [number for number in numbers if number % 2 == 0]
 
 print(squared_numbers)
-
+print(even_numbers)
