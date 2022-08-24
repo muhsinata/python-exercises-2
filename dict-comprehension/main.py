6ecefdbe6d51635db44fcb4252f56b5bb795d442
muhsinata <muhsinatadurak@gmail.com>
2022-08-24 15:47:13 +0300

    feat: convert temp from celcius to fahrenheit

--- dict-comprehension/main.py
+++ dict-comprehension/main.py
@@ -1,6 +1,18 @@
-sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-words = sentence.split()
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# words = sentence.split()
+#
+# number_of_letters = {word: len(word) for word in words}
+#
+# print(number_of_letters)
 
-number_of_letters = {word: len(word) for word in words}
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
 
-print(number_of_letters)
+weather_f = {day: (temp * 9/5) + 32 for (day, temp) in weather_c.items()}
