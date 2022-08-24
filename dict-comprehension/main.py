d113e359f8cc0fe25120fe9a2548196a20e60302
muhsinata <muhsinatadurak@gmail.com>
2022-08-24 16:14:19 +0300

    feat: add phonetic alphabet data file

--- dict-comprehension/main.py
+++ dict-comprehension/main.py
@@ -5,14 +5,26 @@
 #
 # print(number_of_letters)
 
-weather_c = {
-    "Monday": 12,
-    "Tuesday": 14,
-    "Wednesday": 15,
-    "Thursday": 14,
-    "Friday": 21,
-    "Saturday": 22,
-    "Sunday": 24,
-}
+############################################################################
 
-weather_f = {day: (temp * 9/5) + 32 for (day, temp) in weather_c.items()}
+# weather_c = {
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
+#
+# weather_f = {day: (temp * 9/5) + 32 for (day, temp) in weather_c.items()}
+
+############################################################################
+
+import pandas
+
+# TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
