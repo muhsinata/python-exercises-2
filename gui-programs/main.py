1e7f911f53f046b7892404fb255020e6c078c529
muhsinata <muhsinatadurak@gmail.com>
2022-08-25 13:20:23 +0300

    feat: link up button and user input

--- gui-programs/main.py
+++ gui-programs/main.py
@@ -1,20 +1,24 @@
 from tkinter import *
 
+FONT = ("Arial", 10, "normal")
+
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = Label(text="New Text", font=("Courier", 10, "normal"))
+my_label = Label(text="New Text", font=FONT)
 my_label.pack()
 
 
 def button_clicked():
-    my_label.config(text="I got clicked")
+    my_label.config(text=user_input.get())
 
 
 button = Button(text="Click Me", command=button_clicked)
 button.pack()
 
+user_input = Entry(width=10)
+user_input.pack()
 
 
 
