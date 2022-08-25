3d3bd3ca4e706a4ac43472aacc81c8976b46f025
muhsinata <muhsinatadurak@gmail.com>
2022-08-25 13:09:41 +0300

    feat: add a button

--- gui-programs/main.py
+++ gui-programs/main.py
@@ -4,7 +4,16 @@ window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
+my_label = Label(text="New Text", font=("Courier", 10, "normal"))
+my_label.pack()
 
+
+def button_clicked():
+    my_label.config(text="I got clicked")
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 
 
