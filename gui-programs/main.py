3f09e09a9fbe626be194797d49d268bd6a519948
muhsinata <muhsinatadurak@gmail.com>
2022-08-26 14:06:16 +0300

    feat: add miles to kilometer calculator program

--- gui-programs/main.py
+++ gui-programs/main.py
@@ -3,28 +3,32 @@ from tkinter import *
 FONT = ("Arial", 10, "normal")
 
 window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+window.title("Miles to Kilometer Converter")
+window.config(padx=20, pady=20)
 
-my_label = Label(text="New Text", font=FONT)
-my_label.grid(column=0, row=0)
+is_equal_label = Label(text="is equal to", font=FONT)
+is_equal_label.grid(column=0, row=1)
+
+km_label = Label(text="Km", font=FONT)
+km_label.grid(column=2, row=1)
+
+km_result_label = Label(text="0", font=FONT)
+km_result_label.grid(column=1, row=1)
+
+miles_label = Label(text="Miles", font=FONT)
+miles_label.grid(column=2, row=0)
 
 
-# def button_clicked():
-#     my_label.config(text=user_input.get())
+def button_clicked():
+    result = float(miles_input.get()) * 1.6
+    km_result_label.config(text=result)
 
 
-button = Button(text="Click Me")
-button.grid(column=1, row=1)
+button = Button(text="Calculate", command=button_clicked)
+button.grid(column=1, row=2)
 
-button = Button(text="Click Me")
-button.grid(column=2, row=0)
-
-my_label = Label(text="New Text", font=FONT)
-my_label.grid(column=3, row=2)
-
-# user_input = Entry(width=10)
-# user_input.pack()
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
 
 
 
