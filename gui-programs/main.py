95ccaf14e5ca905c9048980a158f214fad7d3e94
muhsinata <muhsinatadurak@gmail.com>
2022-08-26 13:36:15 +0300

    feat: add grid to place buttons and labels

--- gui-programs/main.py
+++ gui-programs/main.py
@@ -7,18 +7,24 @@ window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 my_label = Label(text="New Text", font=FONT)
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 
-def button_clicked():
-    my_label.config(text=user_input.get())
+# def button_clicked():
+#     my_label.config(text=user_input.get())
 
 
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button = Button(text="Click Me")
+button.grid(column=1, row=1)
 
-user_input = Entry(width=10)
-user_input.pack()
+button = Button(text="Click Me")
+button.grid(column=2, row=0)
+
+my_label = Label(text="New Text", font=FONT)
+my_label.grid(column=3, row=2)
+
+# user_input = Entry(width=10)
+# user_input.pack()
 
 
 
